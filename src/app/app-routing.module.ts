0a8c86ef3ef1d83969105340f084d1229739d4de
Aleejandra <mi.currihual@duocuc.cl>
2023-09-20 17:54:06 -0300

    arreglos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login/:username',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
 ];
 
 @NgModule({
