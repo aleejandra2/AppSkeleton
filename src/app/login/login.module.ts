8101c46ff3055d7e099feb76813169b710d8431c
Aleejandra <mi.currihual@duocuc.cl>
2023-09-21 15:27:03 -0300

    mini cambios

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -8,12 +8,22 @@ import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
 
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule
+
   ],
   declarations: [LoginPage]
 })
